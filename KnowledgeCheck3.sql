1024ca40a24ee87af6d4bebb046612cdec076bc0
Alex Hack-Irvin <65615188+AlexIrvin@users.noreply.github.com>
2023-10-29 13:43:42 -0400

    Query Upload

---
+++ KnowledgeCheck3.sql
@@ -0,0 +1,18 @@
+﻿-- Query 1: Customer Name for OrderID 10310
+-- The Big Cheese
+
+SELECT	Orders.OrderID, Customers.CustomerName 
+FROM	Orders
+JOIN	Customers
+ON		Customers.CustomerID = Orders.CustomerID
+WHERE	Orders.OrderID = 10310;
+
+
+-- Query 2: Supplier Address of ProductID 40
+-- Order Processing Dept. 2100 Paul Revere Blvd.
+
+SELECT	Products.ProductID, Suppliers.Address
+FROM	Suppliers
+JOIN	Products
+ON		Suppliers.SupplierID = Products.SupplierID
+WHERE	ProductID = 40;
